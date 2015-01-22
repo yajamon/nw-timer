99f10956d5378fd605fe41a37c60eb05c75453b7
yajamon <yajamon.tatsuki@gmail.com>
2015-01-22 21:52:53 +0900

    Add grunt-typescript

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,11 +19,24 @@ module.exports = function (grunt) {
             }
         },
 
+        typescript: {
+            build: {
+                src: ['src/ts/index.ts'],
+                dest: 'build/js/index.js',
+                options: {
+                    noImplicitAny: true,
+                    target: 'es5',
+                }
+            }
+        },
+
+
     });
 
     // load plugin
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-typescript');
 
     // tasks
     grunt.registerTask('less2css', ['less', 'csslint']);
