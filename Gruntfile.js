3a90a40acffab2ca658f4a8257851ca465b85a21
yajamon <yajamon.tatsuki@gmail.com>
2015-01-22 21:38:51 +0900

    Add grunt.initConfig. less:build

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,15 @@ module.exports = function (grunt) {
     // config
     grunt.initConfig({
 
+        less: {
+            build: {
+                expand: true,
+                cwd: 'src/less/',
+                src: ['**/*.less'],
+                dest: 'build/css/',
+                ext: '.css'
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-less');
