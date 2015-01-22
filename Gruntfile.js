3dad61cf3510f4c8df3e43ee3b5fe5cd00b54d4f
yajamon <yajamon.tatsuki@gmail.com>
2015-01-22 21:40:43 +0900

    Add gruntTask. less to css.

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,4 +25,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-csslint');
 
+    // tasks
+    grunt.registerTask('less2css', ['less', 'csslint']);
+
 };
